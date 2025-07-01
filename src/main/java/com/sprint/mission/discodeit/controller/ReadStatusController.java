356a9dd7c8b81f9e328064ef5c3372050dedcd90
joongwon <anjoongwon517@gmail.com>
2025-07-01 23:16:23 +0900

    feat: 메시지 수신 정보 관리 기능 구현

---
+++ src/main/java/com/sprint/mission/discodeit/controller/ReadStatusController.java
@@ -0,0 +1,51 @@
+package com.sprint.mission.discodeit.controller;
+
+import com.sprint.mission.discodeit.dto.request.MessageUpdateRequest;
+import com.sprint.mission.discodeit.dto.request.ReadStatusCreateRequest;
+import com.sprint.mission.discodeit.dto.request.ReadStatusUpdateRequest;
+import com.sprint.mission.discodeit.service.ReadStatusService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.UUID;
+
+@Controller
+@RequestMapping("/mission4/readStatuses")
+@AllArgsConstructor
+public class ReadStatusController {
+    private final ReadStatusService readStatusService;
+
+    // 특정 채널의 메시지 수신 정보 생성
+    @RequestMapping(value = "/create/{channelId}", method = RequestMethod.POST)
+    public ResponseEntity createReadStatusByChannelId(@PathVariable UUID channelId,
+                                                      @RequestBody ReadStatusCreateRequest request) {
+        System.out.println("######### createMessageStatusByChannelId");
+        System.out.println("# request =" + request);
+
+        return ResponseEntity.ok(readStatusService.create(request));
+    }
+
+    // 특정 채널의 메시지 수신 정보 수정
+    @RequestMapping(value = "/update/{readStatusId}", method = RequestMethod.PATCH)
+    public ResponseEntity updateReadStatusByChannelId(@PathVariable UUID readStatusId,
+                                                      @RequestBody ReadStatusUpdateRequest request) {
+        System.out.println("######### updateMessageStatusByChannelId");
+        System.out.println("# request =" + request);
+
+        return ResponseEntity.ok(readStatusService.update(readStatusId, request));
+    }
+
+    // 특정 사용자의 메시지 수신 정보 조회
+    @RequestMapping(value = "/{userId}", method = RequestMethod.GET)
+    public ResponseEntity findAllReadStatusByUserId(@PathVariable UUID userId) {
+        System.out.println("######### findAllMessageStatusByUserId");
+        System.out.println("# userId =" + userId);
+
+        return ResponseEntity.ok(readStatusService.findAllByUserId(userId));
+    }
+}
