df6eae7712db273f822e6620bea2e2f276edc8db
joongwon <anjoongwon517@gmail.com>
2025-07-03 09:30:19 +0900

    fix: 로그인 시 userStatus update

--- src/main/java/com/sprint/mission/discodeit/service/basic/BasicAuthService.java
+++ src/main/java/com/sprint/mission/discodeit/service/basic/BasicAuthService.java
@@ -2,14 +2,17 @@ package com.sprint.mission.discodeit.service.basic;
 
 import com.sprint.mission.discodeit.dto.data.UserDto;
 import com.sprint.mission.discodeit.dto.request.LoginRequest;
+import com.sprint.mission.discodeit.dto.request.UserStatusUpdateRequest;
 import com.sprint.mission.discodeit.entity.User;
 import com.sprint.mission.discodeit.entity.UserStatus;
 import com.sprint.mission.discodeit.repository.UserRepository;
 import com.sprint.mission.discodeit.repository.UserStatusRepository;
 import com.sprint.mission.discodeit.service.AuthService;
+import com.sprint.mission.discodeit.service.UserStatusService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.time.Instant;
 import java.util.NoSuchElementException;
 
 @RequiredArgsConstructor
@@ -17,6 +20,8 @@ import java.util.NoSuchElementException;
 public class BasicAuthService implements AuthService {
     private final UserRepository userRepository;
     private final UserStatusRepository userStatusRepository;
+
+    private final UserStatusService userStatusService;
 
     @Override
     public UserDto login(LoginRequest loginRequest) {
@@ -29,6 +34,8 @@ public class BasicAuthService implements AuthService {
         if (!user.getPassword().equals(password)) {
             throw new IllegalArgumentException("Wrong password");
         }
+
+        userStatusService.updateByUserId(user.getId(), new UserStatusUpdateRequest(Instant.now()));
 
         return toDto(user);
     }
