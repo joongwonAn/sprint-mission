104158162004d6ce8891bf4eebf60a106795ed0e
joongwon <anjoongwon517@gmail.com>
2025-07-03 09:14:52 +0900

    fix: 로그인 리턴타입 DTO로 변경

--- src/main/java/com/sprint/mission/discodeit/service/UserStatusService.java
+++ src/main/java/com/sprint/mission/discodeit/service/UserStatusService.java
@@ -1,6 +1,5 @@
 package com.sprint.mission.discodeit.service;
 
-import com.sprint.mission.discodeit.dto.data.UserDto;
 import com.sprint.mission.discodeit.dto.request.UserStatusCreateRequest;
 import com.sprint.mission.discodeit.dto.request.UserStatusUpdateRequest;
 import com.sprint.mission.discodeit.entity.UserStatus;
