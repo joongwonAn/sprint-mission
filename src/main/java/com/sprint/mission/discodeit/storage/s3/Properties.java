71c04c372a46484ae09655f997bbefe61e88de9f
joongwon <anjoongwon517@gmail.com>
2025-08-27 08:51:37 +0900

    refactor: presignedUrlExpiration 필드 추가 및 lombok 적용

--- src/main/java/com/sprint/mission/discodeit/storage/s3/Properties.java
+++ src/main/java/com/sprint/mission/discodeit/storage/s3/Properties.java
@@ -1,46 +1,19 @@
 package com.sprint.mission.discodeit.storage.s3;
 
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @ConfigurationProperties(prefix = "discodeit.storage.s3")
+@Getter
+@Setter
 public class Properties {
 
     private String accessKey;
     private String secretKey;
     private String region;
     private String bucket;
-
-    public String getAccessKey() {
-        return accessKey;
-    }
-
-    public void setAccessKey(String accessKey) {
-        this.accessKey = accessKey;
-    }
-
-    public String getSecretKey() {
-        return secretKey;
-    }
-
-    public void setSecretKey(String secretKey) {
-        this.secretKey = secretKey;
-    }
-
-    public String getRegion() {
-        return region;
-    }
-
-    public void setRegion(String region) {
-        this.region = region;
-    }
-
-    public String getBucket() {
-        return bucket;
-    }
-
-    public void setBucket(String bucket) {
-        this.bucket = bucket;
-    }
+    private Long presignedUrlExpiration;
 }
